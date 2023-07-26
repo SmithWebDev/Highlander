f2818183843376584f034c0e462ae45a430e9e28
DB Smith <smithwebdd@gmail.com>
2023-07-25 21:41:11 -0400

    Added animate and lens

---
+++ nvim-0.10/default/lua/smithwebdev/plugins/animate.lua
@@ -0,0 +1,7 @@
+return {
+  'camspiers/animate.vim',  -- https://github.com/camspiers/animate.vim
+  dependencies = {
+    'camspiers/lens.vim',   -- https://github.com/camspiers/lens.vim
+    'psliwka/vim-smoothie', -- https://github.com/psliwka/vim-smoothie
+  },
+}
