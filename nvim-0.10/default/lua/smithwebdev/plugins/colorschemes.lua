3bb0f4310bc6f5cae64c6ab181478b984a94c149
DB Smith <smithwebdd@gmail.com>
2023-07-27 01:14:08 -0400

    Added colorschemes

---
+++ nvim-0.10/default/lua/smithwebdev/plugins/colorschemes.lua
@@ -0,0 +1,34 @@
+return {
+  {'vim-scripts/ScrollColors'},        -- https://github.com/vim-scripts/ScrollColors
+  {'ray-x/starry.nvim'}, -- https://github.com/ray-x/starry.nvim
+  {'yonlu/omni.vim'},                  -- https://github.com/yonlu/omni.vim
+  {'flazz/vim-colorschemes'},          --https://github.com/flazz/vim-colorschemes
+  {'rafi/awesome-vim-colorschemes'},   --https://github.com/rafi/awesome-vim-colorschemes
+  {'numToStr/Sakura.nvim'},            --https://github.com/numToStr/Sakura.nvim
+  --{'hardhackerlabs/themes'},           --https://github.com/hardhackerlabs/themes
+  {
+    'B4mbus/oxocarbon-lua.nvim',       --https://github.com/B4mbus/oxocarbon-lua.nvim
+    init = function()
+      vim.g.oxocarbon_lua_transparent = true
+      vim.g.oxocarbon_lua_alternative_telescope = true
+    end
+  },
+  {
+    'folke/tokyonight.nvim',           --https://github.com/folke/tokyonight.nvim
+    opts = {
+      dim_inactive = true,
+      style = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        floats = 'normal',
+        functions = { italic = true },
+        sidebars = 'normal',
+        variables = {},
+      },
+      sidebars = {},
+      terminal_colors = true,
+      transparent = true,
+    }
+  },
+}
+
