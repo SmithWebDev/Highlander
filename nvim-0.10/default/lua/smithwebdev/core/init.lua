6a63ceb09653f0a291ba30e04cfe6f18cb00f093
DB Smith <smithwebdd@gmail.com>
2023-08-02 23:43:15 -0400

    Adjusted the order for file loading

--- nvim-0.10/default/lua/smithwebdev/core/init.lua
+++ nvim-0.10/default/lua/smithwebdev/core/init.lua
@@ -1,4 +1,4 @@
-require 'smithwebdev.core.autocmds'
-require 'smithwebdev.core.settings'
 require 'smithwebdev.core.keybinds'
+require 'smithwebdev.core.autocmds'
 require 'smithwebdev.core.lazy'
+require 'smithwebdev.core.settings'
