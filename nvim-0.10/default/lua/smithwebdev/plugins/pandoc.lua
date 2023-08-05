06c7110c1bdd1be74bd1030eb75aa509392ba47d
DB Smith <smithwebdd@gmail.com>
2023-08-04 20:13:57 -0400

    Added pandoc and pandoc syntax

---
+++ nvim-0.10/default/lua/smithwebdev/plugins/pandoc.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'vim-pandoc/vim-pandoc', -- https://github.com/vim-pandoc/vim-pandoc
+  },
+  {
+    'vim-pandoc/vim-pandoc-syntax', -- https://github.com/vim-pandoc/vim-pandoc-syntax
+  },
+}
