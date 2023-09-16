729c0503597348c2ba21a0aa5732a51d97d39623
Darrick Smith <smithwebdd@gmail.com>
2023-09-16 14:54:18 -0400

    Added nightfly colorscheme

--- nvim-0.10/default/lua/smithwebdev/plugins/colorschemes.lua
+++ nvim-0.10/default/lua/smithwebdev/plugins/colorschemes.lua
@@ -9,6 +9,14 @@ return {
   'tpope/vim-vividchalk', -- https://github.com/tpope/vim-vividchalk
   { 'freddiehaddad/base16-nvim' },     -- https://github.com/freddiehaddad/base16-nvim
   {
+    "bluz71/vim-nightfly-guicolors",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme nightfly]])
+    end,
+  },
+  {
     'B4mbus/oxocarbon-lua.nvim',       --https://github.com/B4mbus/oxocarbon-lua.nvim
     init = function()
       vim.g.oxocarbon_lua_transparent = true
@@ -17,20 +25,36 @@ return {
   },
   {
     'folke/tokyonight.nvim',           --https://github.com/folke/tokyonight.nvim
-    opts = {
-      dim_inactive = true,
-      style = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        floats = 'normal',
-        functions = { italic = true },
-        sidebars = 'normal',
-        variables = {},
-      },
-      sidebars = {},
-      terminal_colors = true,
-      transparent = true,
-    }
+    config = function ()
+      require('tokyonight').setup({
+        style = 'night',
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          floats = 'normal',
+          functions = { italic = true },
+          sidebars = 'normal',
+          variables = {},
+        },
+        dim_inactive = true,
+      })
+    end,
+    --opts = {
+    --  dim_inactive = true,
+    --  styles = {
+    --    comments = { italic = true },
+    --    keywords = { italic = true },
+    --    floats = 'normal',
+    --    functions = { italic = true },
+    --    sidebars = 'normal',
+    --    variables = {},
+    --  },
+    --  sidebars = {},
+    --  terminal_colors = true,
+    --  transparent = true,
+    --}
   },
   { 'ldelossa/vimdark' },                                 -- https://github.com/ldelossa/vimarkd
   --vim.cmd.colorscheme 'aquamarine'
